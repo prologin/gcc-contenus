0505e8a325186f4dc5d8910d80d7c3b2b7256b20
Tanguy Segarra <tanguy.segarra@epita.fr>
2019-12-16 16:38:01 +0100

    matrices: add python test suite

---
+++ 04_matrices/tests-matrices.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+import matrices as tp
+
+m1 = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+     ]
+
+m2 = [
+        [42,42],
+        [42,42],
+        [42,42]
+     ]
+
+mDiag = [
+        [1,2,3],
+        [2,1,5],
+        [3,5,1]
+     ]
+
+mnm = [
+        [11, 42, 39],
+        [12, 15, 86],
+        [98, 56, 27]
+      ]
+
+
+def test(f, inp, out):
+    inpp = ", ".join(str(x) for x in inp)
+    print(f"{f.__name__}({inpp}) =", end=" ")
+    try:
+        res = f(*inp)
+        print(f"{res}", end=" ")
+        if res == out:
+            print("\033[0;32m [OK] \033[0m")
+        else:
+            print(f"| Attendu: {out} \033[0;31m [KO]\033[0m")
+    except Exception as e:
+        print(e, "\033[0;31m [K0] \033[0m")
+
+
+# Exercice 1 : initialiser une matrice
+try:
+    test(tp.initMat, [3, 2, 42], [[42,42],[42,42],[42,42]])
+    test(tp.initMat, [2, 2, 2], [[2,2],[2,2]])
+    test(tp.initMat, [0, 2, 1], [])
+    test(tp.initMat, [3, 0, 5], [[],[],[]])
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 3 : trace
+try:
+    test(tp.trace, [m1], 15)
+    test(tp.trace, [m2], None)
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 4 : ajouter deux matrices
+try:
+    test(tp.addMat, [m1, m1], [[2,4,6],[8,10,12],[14,16,18]])
+    test(tp.addMat, [m1, m2], None)
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 5 : symetrie sur la diagonale
+try:
+    test(tp.symmetricDiag, [m1], False)
+    test(tp.symmetricDiag, [mDiag], True)
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 6 : minimax
+try:
+    test(tp.minimax, [mnm], 42)
+except Exception as e:
+    print(e)
+
+print()
