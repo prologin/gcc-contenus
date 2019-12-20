97c82d8a0c1a4aab2f2d744154c1240a806366c7
Tanguy Segarra <tanguy.segarra@epita.fr>
2019-12-20 12:53:20 +0100

    matrices: add code template for attendees

--- 04_matrices/matrices.py
+++ 04_matrices/matrices.py
@@ -3,120 +3,39 @@ Exercice 1 : initialiser une matrice
 '''
 
 def initMat(lines, cols, value):
-
-    M = []
-
-    for i in range(lines):
-        M.append([])
-        for j in range(cols):
-            M[i].append(value)
-
-    return M
+    pass
 
 '''
 Exercice 2 : afficher une matrice
 '''
 
 def printMat(M):
-
-    lines = len(M)
-    cols = len(M[0])
-
-    for i in range(lines):
-        for j in range(cols):
-            print(M[i][j], end=" ")
-
-        print()
+    pass
 
 '''
 Exercice 3 : laisser une trace
 '''
 
 def trace(M):
-
-    trace = 0
-    longueurLigne = len(M[0])
-
-    if longueurLigne != len(M):
-        return None;
-
-    for i in range(longueurLigne):
-        trace += M[i][i]
-
-    return trace
+    pass
 
 '''
 Exercice 4 : ajouter deux matrices
 '''
 
 def addMat(A, B):
-
-    (lines, cols) = (len(A), len(A[0]))
-
-    if (lines, cols) != (len(A), len(B[0])):
-        return None
-
-    M = initMat(lines, cols, 0)
-
-    for i in range(lines):
-        for j in range(cols):
-            M[i][j] = A[i][j] + B[i][j]
-
-    return M
+    pass
 
 '''
 Exercice 5 : symetrie sur la diagonale
 '''
 
 def symmetricDiag(M):
-
-    l = len(M)
-    c = len(M[0])
-
-    if l != c:
-        return False
-
-    i = 0
-
-    while i < l:
-        j = 0
-        while j < c:
-            if M[i][j] != M[j][i]:
-                return False
-
-            j += 1
-
-        i += 1
-
-    return True
+    pass
 
 '''
 Exercice 6 : minimax
 '''
 
-def maxList(L):
-
-    maxi = L[0]
-
-    for i in range(len(L)):
-        if L[i] > maxi:
-            maxi = L[i]
-
-    return maxi
-
-def mini(a, b):
-
-    if (a < b):
-        return a
-    else:
-        return b
-
 def minimax(M):
-
-    mnm = maxList(M[0])
-
-    for i in range(len(M)):
-        mnm = mini(mnm, maxList(M[i]))
-
-    return mnm
-
+    pass
