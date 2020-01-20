9043ac465d39ff653487cd6867ba08487d7e36a2
Tanguy Segarra <tanguy.segarra@epita.fr>
2020-01-20 11:15:00 +0100

    matrices: update test suite for cleaner output

--- 04_matrices/tests-matrices.py
+++ 04_matrices/tests-matrices.py
@@ -13,7 +13,6 @@ tp = importlib.import_module(tp)
 
 
 def load_map(filename):
-
     M = []
     f = open(filename)
     lines = f.readlines()
@@ -23,6 +22,23 @@ def load_map(filename):
         M.append(l)
 
     return M;
+
+def test(f, inp, out):
+
+    inpp = ", ".join(str(x) for x in inp)
+
+    try:
+        res = f(*inp)
+        if res == out:
+            print("\033[0;32m[OK]\033[0m")
+        else:
+            if f.__name__ == "eat" or f.__name__ == "craters":
+                print(f"\033[0;31m[K0]\033[0m Recu : {res} | Attendu : {out}")
+            else:
+                print(f"{f.__name__}({inpp}) = {res} | Attendu : {out} \033[0;31m[KO]\033[0m")
+
+    except Exception as e:
+        print(e, "\033[0;31m[K0]\033[0m")
 
 
 m1 = [
@@ -50,25 +66,8 @@ mnm = [
       ]
 
 
-def test(f, inp, out):
-
-    inpp = ", ".join(str(x) for x in inp)
-
-    try:
-        res = f(*inp)
-        if res == out:
-            print("\033[0;32m[OK]\033[0m")
-        else:
-            print(f"{f.__name__}({inpp}) =", end=" ")
-            print(f"{res}", end=" ")
-            print(f"| Attendu: {out} \033[0;31m[KO]\033[0m")
-
-    except Exception as e:
-        print(e, "\033[0;31m[K0]\033[0m")
-
-
-print("Exercice 1 : présence dans une matrice")
 # Exercice 1 : présence dans une matrice
+print("Exercice 1 : présence dans une matrice")
 try:
     test(tp.is_in_matrix, [m1, 1], True)
     test(tp.is_in_matrix, [m1, 5], True)
@@ -81,8 +80,8 @@ except Exception as e:
 print()
 
 
-print("Exercice 2 : coordonnées dans une matrice")
 # Exercice 2 : coordonnées dans une matrice
+print("Exercice 2 : coordonnées dans une matrice")
 try:
     test(tp.search_matrix, [m1, 1], [0, 0])
     test(tp.search_matrix, [m1, 5], [1, 1])
@@ -94,8 +93,8 @@ except Exception as e:
 print()
 
 
-print("Exercice 3 : toutes les coordonnées !")
 # Exercice 3 : toutes les coordonnées !
+print("Exercice 3 : toutes les coordonnées !")
 try:
     test(tp.search_all, [mDiag, 1], [[0, 0], [1, 1], [2, 2]])
     test(tp.search_all, [mDiag, 5], [[1, 2], [2, 1]])
@@ -109,8 +108,8 @@ print()
 print()
 
 
-print("Exercice 4 : initialiser une matrice")
 # Exercice 4 : initialiser une matrice
+print("Exercice 4 : initialiser une matrice")
 try:
     test(tp.set_matrix, [3, 2, 42], [[42,42],[42,42],[42,42]])
     test(tp.set_matrix, [2, 2, 2], [[2,2],[2,2]])
@@ -123,8 +122,8 @@ except Exception as e:
 print()
 
 
-print("Exercice 6 : somme des éléments de la matrice")
 # Exercice 6 : somme des éléments de la matrice
+print("Exercice 6 : somme des éléments de la matrice")
 try:
     test(tp.sum_matrix, [m1], 45)
     test(tp.sum_matrix, [m2], 252)
@@ -136,8 +135,8 @@ except Exception as e:
 print()
 
 
-print("Exercice 7 : ajouter deux matrices")
 # Exercice 7 : ajouter deux matrices
+print("Exercice 7 : ajouter deux matrices")
 try:
     test(tp.add_matrix, [m1, m1], [[2,4,6],[8,10,12],[14,16,18]])
     test(tp.add_matrix, [m1, m2], None)
@@ -148,8 +147,8 @@ except Exception as e:
 print()
 
 
-print("Exercice 8 : symetrie sur la diagonale")
 # Exercice 8 : symetrie sur la diagonale
+print("Exercice 8 : symetrie sur la diagonale")
 try:
     test(tp.symmetricDiag, [m1], False)
     test(tp.symmetricDiag, [mDiag], True)
@@ -160,8 +159,8 @@ except Exception as e:
 print()
 
 
-print("Exercice 9 : minimax")
 # Exercice 9 : minimax
+print("Exercice 9 : minimax")
 try:
     test(tp.minimax, [mnm], 42)
 
@@ -171,8 +170,8 @@ except Exception as e:
 print()
 
 
-print("Exercice 10 : une faim de loup")
 # Exercice 10 : une faim de loup
+print("Exercice 10 : une faim de loup")
 try:
     test(tp.eat, [load_map("maps/wolf1.map")], 3)
     test(tp.eat, [load_map("maps/wolf2.map")], 0)
@@ -184,8 +183,8 @@ except Exception as e:
 print()
 
 
-print("Exercice 11 : les cratères de la Lune")
 # Exercice 11 : les cratères de la Lune
+print("Exercice 11 : les cratères de la Lune")
 try:
     test(tp.craters, [load_map("maps/moon1.map")], 0)
     test(tp.craters, [load_map("maps/moon2.map")], 1)
