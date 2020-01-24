8a4e521d28c38849520364b429fab5d1cc41f338
Tanguy Segarra <tanguy.segarra@epita.fr>
2020-01-24 14:48:00 +0100

    matrices: update given file functions

--- 04_matrices/matrices.py
+++ 04_matrices/matrices.py
@@ -1,41 +1,75 @@
 '''
-Exercice 1 : initialiser une matrice
+Exercice 1 : présence dans une matrice
 '''
 
-def initMat(lines, cols, value):
+def is_in_matrix(M, val):
     pass
 
 '''
-Exercice 2 : afficher une matrice
+Exercice 2 : coordonnées dans une matrice
 '''
 
-def printMat(M):
+def search_matrix(M, val):
     pass
 
 '''
-Exercice 3 : laisser une trace
+Exercice 3 : toutes les coordonnées !
 '''
 
-def trace(M):
+def search_all(M, val):
     pass
 
 '''
-Exercice 4 : ajouter deux matrices
+Exercice 4 : initialiser une matrice
 '''
-
-def addMat(A, B):
+def set_matrix(lines, cols, val):
     pass
 
 '''
-Exercice 5 : symetrie sur la diagonale
+Exercice 5 : afficher une matrice
+'''
+
+def print_matrix(M):
+    pass
+
+'''
+Exercice 6 : somme des éléments de la matrice
+'''
+
+def sum_matrix(M):
+    pass
+
+'''
+Exercice 7 : ajouter deux matrices
+'''
+
+def add_matrix(A, B):
+    pass
+
+'''
+Exercice 8 : symetrie sur la diagonale
 '''
 
 def symmetricDiag(M):
     pass
 
 '''
-Exercice 6 : minimax
+Exercice 9 : minimax
 '''
 
 def minimax(M):
+    pass
+
+'''
+Exercice 10 : une faim de loup
+'''
+
+def eat(M):
+    pass
+
+'''
+Exercice 11 : les cratères de la Lune
+'''
+
+def craters(M):
     pass
