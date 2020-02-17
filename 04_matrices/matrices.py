3af07ef9bec99808adce49ffa584896ac774248a
Tanguy Segarra <tanguy.segarra@epita.fr>
2020-02-17 14:35:50 +0100

    matrices: add functions descriptions in code template

--- 04_matrices/matrices.py
+++ 04_matrices/matrices.py
@@ -1,32 +1,51 @@
 '''
 Exercice 1 : présence dans une matrice
+
+Ecrire la fonction `is_in_matrix(P, val)` qui renvoie `True` si la valeur 'val'
+est présente dans la matrice, `False` sinon.
 '''
 
-def is_in_matrix(M, val):
+def is_in_matrix(P, val):
     pass
 
 '''
 Exercice 2 : coordonnées dans une matrice
+
+Ecrire la fonction `search_matrix(P, val)` qui renvoie une liste contenant deux
+éléments, l'indice de la ligne et l'indice de la colonne sur lesquelles se
+trouve la valeur recherchée 'val'.
 '''
 
-def search_matrix(M, val):
+def search_matrix(P, val):
     pass
 
 '''
 Exercice 3 : toutes les coordonnées !
+
+Ecrire la fonction `search_all(P, val)` qui renvoie une liste contenant tous
+les duos de coordonnées sur lesquelles on peut trouver la valeur recherchée
+'val'.
 '''
 
-def search_all(M, val):
+def search_all(P, val):
     pass
 
 '''
 Exercice 4 : initialiser une matrice
+
+Ecrire la fonction `set_matrix(lines, cols, val)` qui crée une matrice
+de dimensions 'lines' par 'cols', parcourt la matrice et met chaque case de
+cette matrice à la valeur 'val'.
 '''
 def set_matrix(lines, cols, val):
     pass
 
 '''
 Exercice 5 : afficher une matrice
+
+Ecrire la fonction `print_matrix(M)` qui affichera la matrice prise en
+paramètre en séparant chaque terme affiché par un espace, et un retour à la
+ligne en fin de ligne de matrice.
 '''
 
 def print_matrix(M):
@@ -34,6 +53,9 @@ def print_matrix(M):
 
 '''
 Exercice 6 : somme des éléments de la matrice
+
+Ecrire la fonction `sum_matrix(M)` qui prend une matrice en paramètre,
+et renvoie la somme de tous ses éléments.
 '''
 
 def sum_matrix(M):
@@ -41,6 +63,11 @@ def sum_matrix(M):
 
 '''
 Exercice 7 : ajouter deux matrices
+
+Ecrire la fonction `add_matrix(a, b)` qui ajoute les valeurs des deux matrices
+a et b dans une nouvelle matrice **si et seulement si** elles sont de mêmes
+dimensions.  Si les dimensions sont invalides, votre fonction doit renvoyer la
+valeur `None`, au lieu de renvoyer la matrice nouvellement créée.
 '''
 
 def add_matrix(A, B):
@@ -48,6 +75,10 @@ def add_matrix(A, B):
 
 '''
 Exercice 8 : symetrie sur la diagonale
+
+Ecrire la fonction `sym_diago(m)` qui renvoie `True` si la matrice est
+symétrique par rapport à l'axe de sa diagonale (du coin en haut à gauche, à son
+coin en bas à droite), `False` sinon.
 '''
 
 def symmetricDiag(M):
@@ -55,6 +86,9 @@ def symmetricDiag(M):
 
 '''
 Exercice 9 : minimax
+
+Ecrire la fonction `minimax(m)` qui détermine la valeur minimale parmi les
+maximums de chaque ligne de la matrice passée en paramètre.
 '''
 
 def minimax(M):
@@ -62,13 +96,19 @@ def minimax(M):
 
 '''
 Exercice 10 : une faim de loup
+
+Ecrire la fonction `eat(P)` qui prend une matrice représentant le terrain en
+paramètre et renvoie le nombre de moutons que va pouvoir manger le loup.
 '''
 
-def eat(M):
+def eat(P):
     pass
 
 '''
 Exercice 11 : les cratères de la Lune
+
+Ecrire la fonction `craters(m)` qui compte le nombre de cratères dessinés dans
+la matrice passée en argument.
 '''
 
 def craters(M):
