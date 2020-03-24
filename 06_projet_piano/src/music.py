ca2f5e6da188a1b0528d7aa25e57877bb1e3cea9
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-03-24 21:13:43 +0100

    music: use classes for key states instead of tuples

--- 06_projet_piano/src/music.py
+++ 06_projet_piano/src/music.py
@@ -10,6 +10,13 @@ with contextlib.redirect_stdout(None):
     import pygame.midi
 
 
+class Key:
+    def __init__(self, pitch, is_black):
+        self.pitch = pitch
+        self.is_black = is_black
+        self.on = False
+
+
 # Colors for keys and background
 color_grey  = (127, 127, 127)
 color_white = (255, 255, 255)
@@ -29,18 +36,18 @@ key_to_note = {
         # on: a boolean to keep track of if this note is on
 
         # 12 is the pitch for C0 (this isn't on a piano keyboard as they start at A0 (pitch: 21))
-        pygame.K_a: (12, False, False),  # C0       (azerty: pygame.K_q)
-        pygame.K_w: (13, True,  False),  # C#/Db0   (azerty: pygame.K_z)
-        pygame.K_s: (14, False, False),  # D0
-        pygame.K_e: (15, True, False),   # D#/Eb0
-        pygame.K_d: (16, False,  False), # E0
-        pygame.K_f: (17, False, False),  # F0
-        pygame.K_t: (18, True,  False),  # F#/Gb0
-        pygame.K_g: (19, False, False),  # G0
-        pygame.K_y: (20, True, False),   # G#/Ab0
-        pygame.K_h: (21, False,  False), # A0
-        pygame.K_u: (22, True, False),   # A#/Bb0
-        pygame.K_j: (23, False,  False), # B0
+        pygame.K_a: Key(12, False), # C0       (azerty: pygame.K_q)
+        pygame.K_w: Key(13, True),  # C#/Db0   (azerty: pygame.K_z)
+        pygame.K_s: Key(14, False), # D0
+        pygame.K_e: Key(15, True),  # D#/Eb0
+        pygame.K_d: Key(16, False), # E0
+        pygame.K_f: Key(17, False), # F0
+        pygame.K_t: Key(18, True),  # F#/Gb0
+        pygame.K_g: Key(19, False), # G0
+        pygame.K_y: Key(20, True),  # G#/Ab0
+        pygame.K_h: Key(21, False), # A0
+        pygame.K_u: Key(22, True),  # A#/Bb0
+        pygame.K_j: Key(23, False), # B0
 }
 
 # The number of different notes in an octave
@@ -88,10 +95,10 @@ def draw_keyboard(window) -> None:
 
     # Draw white keys
     for key in key_to_note:
-        note, black, on = key_to_note[key]
-        if black:
+        note = key_to_note[key]
+        if note.is_black:
             continue
-        pygame.draw.rect(window, color_red if on else color_white, (left, top, white_width, bottom - top))
+        pygame.draw.rect(window, color_red if note.on else color_white, (left, top, white_width, bottom - top))
         left += margin + white_width
 
     # Reset left for black keys
@@ -101,12 +108,12 @@ def draw_keyboard(window) -> None:
 
     # Draw black keys
     for key in key_to_note:
-        note, black, on = key_to_note[key]
-        if not black:
+        note = key_to_note[key]
+        if not note.is_black:
             continue
-        if note == 18:  # Skip the inexistant black key between E and F
+        if note.pitch == 18:  # Skip the inexistant black key between E and F
             left += margin + white_width
-        pygame.draw.rect(window, color_blue if on else color_black, (left, top, black_width, bottom - top))
+        pygame.draw.rect(window, color_blue if note.on else color_black, (left, top, black_width, bottom - top))
         left += margin + white_width
 
 def main() -> None:
@@ -150,13 +157,13 @@ def main() -> None:
             if event.type == pygame.KEYDOWN:
                 key = event.key
                 if key in key_to_note:
-                    note, black, on = key_to_note[key]
-                    if not on:
+                    note = key_to_note[key]
+                    if not note.on:
                         # XXX: change this once pygame merges PR #1561
-                        print(f'DOWN: Key: {key} Note: {note + note_count * octave} ', end='')
-                        print(f'Name: {midi_to_ansi_note(note + note_count * octave)}')
-                        midi_hndl.note_on(note + note_count * octave, velocity)
-                        key_to_note[key] = note, black, True
+                        print(f'DOWN: Key: {key} Note: {note.pitch + note_count * octave} ', end='')
+                        print(f'Name: {midi_to_ansi_note(note.pitch + note_count * octave)}')
+                        midi_hndl.note_on(note.pitch + note_count * octave, velocity)
+                        note.on = True
 
                 # Exit is user pressed <escape>
                 elif key == pygame.K_ESCAPE:
@@ -166,13 +173,13 @@ def main() -> None:
             elif event.type == pygame.KEYUP:
                 key = event.key
                 if key in key_to_note:
-                    note, black, on = key_to_note[key]
-                    if on:
+                    note = key_to_note[key]
+                    if note.on:
                         # XXX: change this once pygame merges PR #1561
-                        print(f'UP: Key: {key} Note: {note + note_count * octave} ', end='')
-                        print(f'Name: {midi_to_ansi_note(note + note_count * octave)}')
-                        midi_hndl.note_off(note + note_count * octave)
-                        key_to_note[key] = note, black, False
+                        print(f'UP: Key: {key} Note: {note.pitch + note_count * octave} ', end='')
+                        print(f'Name: {midi_to_ansi_note(note.pitch + note_count * octave)}')
+                        midi_hndl.note_off(note.pitch + note_count * octave)
+                        note.on = False
 
         draw_keyboard(window)
         pygame.display.update()
