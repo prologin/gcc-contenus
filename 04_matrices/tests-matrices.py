f471941160b9aec7ec3eaf002ccdd0201b88e82d
Tanguy Segarra <tanguy.segarra@epita.fr>
2020-04-03 14:32:41 +0200

    Matrices (#8)
    * matrices: add mars craters exercise reference
    
    * matrices: add first draft of subject
    
    * matrices: add part of ref for matrices subject
    
    * matrices: update subject (lacks course + last exercise)
    
    * matrices: update ref
    
    * matrices: update craters ref to handle a passed map as argument + fix typo
    
    * matrices: add maps example for craters exercise
    
    * matrices: update craters exercise
    
    * matrices: update exercises names
    
    * matrices: renamed reference solution for craters exercise
    
    * matrices: quick update on craters exercise
    
    * matrices: replace mars by moon in both template and ref for craters exercise
    
    * matrices: remove matrix creation while addition (Nicolas's input)
    
    * matrices: update ref for craters exercise (much cleaner)
    
    * matrices: update craters subject
    
    * matrices: add map with crater on the edge for last exercise
    
    * matrices: add boundaries check in craters refernce
    
    * matrices: add python test suite
    
    * matrices: fix ref issues
    
    * matrices: subject: fix demanded return values
    
    * matrices: fix accents and typos
    
    * matrices: add some course for matrices
    
    * matrices: tests: testsuite now accepts a file given as command line argument
    
    * matrices: rename ref file
    
    * matrices: add code template for attendees
    
    * matrices: test: add tests for searchMat
    
    * matrices: ref: add searchMat function
    
    * matrices: add searchMat exercise + fix typos
    
    * matrices: add 3 progressive exercises about searching in matrices
    
    * matrices: add faim de loup exercise
    
    * matrices: big refacto on subject
    
    * matrices: update subject
    
    * matrices: remove craters exercise template for attendees
    
    * matrices: rename moon maps for last exercise
    
    * matrices: add maps for wolf eating exercise
    
    * matrices: move craters' ref into global ref
    
    * matrices: update ref with new exercises and craters
    
    * matrices: update tests with new exercises
    
    * matrices: add check on argc in tests
    
    * matrices: update test suite for cleaner output
    
    * matrices: fix markdown typo
    
    * matrices: update given file functions
    
    * matrices: fix testsuite file coding style
    
    * matrices: add tips in subject
    
    * matrices: add functions descriptions in code template
    
    * matrices: optimize ref
    
    * matrices: fix minimax subject according to Garance's review
    
    * matrices: add count exercise
    
    * matrices: apply haltode reviews + add count exercise
    
    * matrices: minor fixes
    
    * matrices: Add images
    
    Co-authored-by: fnareoh <garance.gourdel@laposte.net>

---
+++ 04_matrices/tests-matrices.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python3
+
+import sys, importlib
+
+if len(sys.argv) == 1:
+    print("Usage: " + sys.argv[0] + " <ton_fichier>")
+    sys.exit(2)
+
+tp = sys.argv[1]
+if tp[-3:] == ".py":
+    tp = tp[:-3]
+tp = importlib.import_module(tp)
+
+
+def load_map(filename):
+    M = []
+    f = open(filename)
+    lines = f.readlines()
+    f.close()
+
+    for l in lines:
+        M.append(l)
+
+    return M;
+
+def test(f, inp, out):
+
+    inpp = ", ".join(str(x) for x in inp)
+
+    try:
+        res = f(*inp)
+        if res == out:
+            print("\033[0;32m[OK]\033[0m")
+        else:
+            if f.__name__ == "eat" or f.__name__ == "craters":
+                print(f"\033[0;31m[K0]\033[0m Recu : {res} | Attendu : {out}")
+            else:
+                print(f"{f.__name__}({inpp}) = {res} | Attendu : {out}", end="")
+                print(f" \033[0;31m[KO]\033[0m")
+
+    except Exception as e:
+        print(e, "\033[0;31m[K0]\033[0m")
+
+
+m1 = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+     ]
+
+m2 = [
+        [42,42],
+        [42,42],
+        [42,42]
+     ]
+
+mDiag = [
+        [1,2,3],
+        [2,1,5],
+        [3,5,1]
+     ]
+
+mnm = [
+        [11, 42, 39],
+        [12, 15, 86],
+        [98, 56, 27]
+      ]
+
+
+# Exercice 1 : présence dans une matrice
+print("Exercice 1 : présence dans une matrice")
+try:
+    test(tp.is_in_matrix, [m1, 1], True)
+    test(tp.is_in_matrix, [m1, 5], True)
+    test(tp.is_in_matrix, [m1, 69], False)
+    test(tp.is_in_matrix, [m1, 42], False)
+
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 2 : coordonnées dans une matrice
+print("Exercice 2 : coordonnées dans une matrice")
+try:
+    test(tp.search_matrix, [m1, 1], [0, 0])
+    test(tp.search_matrix, [m1, 5], [1, 1])
+    test(tp.search_matrix, [m1, 8], [2, 1])
+
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 3 : toutes les coordonnées !
+print("Exercice 3 : toutes les coordonnées !")
+try:
+    test(tp.search_all, [mDiag, 1], [[0, 0], [1, 1], [2, 2]])
+    test(tp.search_all, [mDiag, 5], [[1, 2], [2, 1]])
+    test(tp.search_all, [mDiag, 42], [])
+
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 4 : initialiser une matrice
+print("Exercice 4 : initialiser une matrice")
+try:
+    test(tp.set_matrix, [3, 2, 42], [[42,42],[42,42],[42,42]])
+    test(tp.set_matrix, [2, 2, 2], [[2,2],[2,2]])
+    test(tp.set_matrix, [0, 2, 1], [])
+    test(tp.set_matrix, [3, 0, 5], [[],[],[]])
+
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 5 : compter les moutons...
+print("Exercice 5 : compter les moutons...")
+try:
+    test(tp.count, [mDiag, 1], 3)
+    test(tp.count, [m2, 42], 6)
+    test(tp.count, [mDiag, 69], 0)
+
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 7 : somme des éléments de la matrice
+print("Exercice 7 : somme des éléments de la matrice")
+try:
+    test(tp.sum_matrix, [m1], 45)
+    test(tp.sum_matrix, [m2], 252)
+    test(tp.sum_matrix, [mnm], 386)
+
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 8 : ajouter deux matrices
+print("Exercice 8 : ajouter deux matrices")
+try:
+    test(tp.add_matrix, [m1, m1], [[2,4,6],[8,10,12],[14,16,18]])
+    test(tp.add_matrix, [m1, m2], None)
+
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 9 : symetrie sur la diagonale
+print("Exercice 9 : symetrie sur la diagonale")
+try:
+    test(tp.symmetricDiag, [m1], False)
+    test(tp.symmetricDiag, [mDiag], True)
+
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 10 : minimax
+print("Exercice 10 : minimax")
+try:
+    test(tp.minimax, [mnm], 42)
+
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 11 : une faim de loup
+print("Exercice 12 : une faim de loup")
+try:
+    test(tp.eat, [load_map("maps/wolf1.map")], 3)
+    test(tp.eat, [load_map("maps/wolf2.map")], 0)
+    test(tp.eat, [load_map("maps/wolf3.map")], 0)
+
+except Exception as e:
+    print(e)
+
+print()
+
+
+# Exercice 12 : les cratères de la Lune
+print("Exercice 12 : les cratères de la Lune")
+try:
+    test(tp.craters, [load_map("maps/moon1.map")], 0)
+    test(tp.craters, [load_map("maps/moon2.map")], 1)
+    test(tp.craters, [load_map("maps/moon3.map")], 2)
+    test(tp.craters, [load_map("maps/moon4.map")], 3)
+
+except Exception as e:
+    print(e)
+
+print()
+
+sys.exit(0)
