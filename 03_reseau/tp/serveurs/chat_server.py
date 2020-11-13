592856708f31699d1b6ec2e46fc44ba829fde278
Augustin Thiercelin <augustin.thiercelin@outlook.com>
2020-11-13 20:34:45 +0100

    tp_reseau: discard spam messages instead of delaying

--- 03_reseau/tp/serveurs/chat_server.py
+++ 03_reseau/tp/serveurs/chat_server.py
@@ -3,6 +3,8 @@ import time
 
 messages = []
 new_message = asyncio.Condition()
+
+# Contains list of (ip, pseudo, time_spam)
 spammers = []
 
 # Rate limit = `RATE_MESSAGE` / `RATE_SECONDS`
@@ -39,16 +41,29 @@ async def _handle_rclient(client_reader, client_writer):
         if (len(msg.strip()) == 0):
             continue
 
+        # check if the client is a spammer
+        if any(peername in spammer for spammer in spammers):
+            current_time = time.time()
+            (ip, pseudo, spam_time) = [(ip, pseudo, spam_time)
+                                       for ip, pseudo, spam_time in spammers
+                                       if ip == peername][0]
+            # Spam limitation is over
+            if (current_time >= spam_time + RATE_SECONDS):
+                spammers.remove((ip, pseudo, spam_time))
+            # Current limitation prevents to send messages
+            else:
+                continue
+
         # spam detection routine
         if messages == RATE_MESSAGE:
             current_time = time.time()
             if (current_time - last_time) < RATE_SECONDS:
-                spammers.append((peername, nickname))
+                spammers.append((peername, nickname, current_time))
                 print(f'{nickname} ({peername}) is spamming.')
                 async with new_message:
+                    # Notify to send a warning message to spammer
                     new_message.notify_all()
-                await asyncio.sleep(RATE_SECONDS - (current_time - last_time))
-                spammers.remove((peername, nickname))
+                continue
             messages = 0
             last_time = current_time
         messages += 1
@@ -67,14 +82,27 @@ async def _handle_wclient(client_reader, client_writer):
     if socket is not None:
         peername = socket.getpeername()[0]
 
+    spam_time = 0
+    already_warned = 0
+
     while True:
         async with new_message:
             await new_message.wait()
 
+            # Check if client is a spammer
             if any(peername in spammer for spammer in spammers):
-                client_writer.write(bytes(
-                    "Spam détecté ! (Plus de 10 messages par secondes)\n",
-                    encoding='utf-8'))
+                if spam_time > (time.time() + RATE_SECONDS):
+                    # The client is not anymore restricted, reset flag
+                    already_warned = 0
+
+                # Send him a warning if it is the first time
+                if not already_warned:
+                    client_writer.write(bytes(
+                        f"Spam détecté ! (Plus de {RATE_MESSAGE} messages"
+                        f"en {RATE_SECONDS} secondes)\n",
+                        encoding='utf-8'))
+                    already_warned = 1
+                    spam_time = time.time()
 
             for msg in messages[i:len(messages)]:
                 client_writer.write(bytes(msg, encoding='utf-8'))
