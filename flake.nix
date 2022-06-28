984b41dd3508602591b313ec3fd80fbc3cf09334
Alexandre Voinier <alexandrevoinier34@gmail.com>
2022-06-28 21:39:43 +0200

    Setup Hugo + gitlab-ci

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    futils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, futils } @ inputs:
+    let
+      inherit (nixpkgs) lib;
+      inherit (lib) recursiveUpdate;
+      inherit (futils.lib) eachDefaultSystem defaultSystems;
+
+      nixpkgsFor = lib.genAttrs defaultSystems (system: import nixpkgs {
+        inherit system;
+      });
+    in
+    (eachDefaultSystem (system:
+      let
+        pkgs = nixpkgsFor.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            hugo
+          ];
+        };
+      }
+    ));
+}
